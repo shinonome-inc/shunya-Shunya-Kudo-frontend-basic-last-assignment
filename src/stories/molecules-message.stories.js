94de4006f5a98d941af52ac15a83509ad243ca35
ssshun3 <s.kudo.22.7.584@4nonome.com>
2022-08-19 03:36:07 +0900

    順番を変える

---
+++ src/stories/molecules-message.stories.js
@@ -0,0 +1,8 @@
+import Item_Message from "../components/molecules/message/message.html";
+import "../components/molecules/message/message.scss";
+
+
+export default { title: "Molecules/Message" }
+
+
+export const Message = () => Item_Message;
