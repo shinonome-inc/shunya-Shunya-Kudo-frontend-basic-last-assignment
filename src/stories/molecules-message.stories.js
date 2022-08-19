fb62d3e11cf8fb12276cfb5d545513b1feabce89
ssshun3 <s.kudo.22.7.584@4nonome.com>
2022-08-19 23:12:53 +0900

    コミット１

---
+++ src/stories/molecules-message.stories.js
@@ -0,0 +1,6 @@
+import Item_Message from "../components/molecules/message/message.html";
+import "../components/molecules/message/message.scss";
+
+export default { title: "Mpolecules/Message" }
+
+export const Example = () => Item_Message;
