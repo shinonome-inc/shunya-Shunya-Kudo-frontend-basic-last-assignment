7f1a4a7b74fcc4c50744c14bd6a482d6f40b9231
ssshun3 <s.kudo.22.7.584@4nonome.com>
2022-08-19 23:19:13 +0900

    Example→Message

--- src/stories/molecules-message.stories.js
+++ src/stories/molecules-message.stories.js
@@ -1,6 +1,6 @@
-import Item_Message from "../components/molecules/message/message.html";
+import Item_Message from "../components/molecukes/message/message.html";
 import "../components/molecules/message/message.scss";
 
-export default { title: "Mpolecules/Message" }
+export default { title: "Molecules/Message" }
 
-export const Example = () => Item_Message;
+export const Message = () => Item_Message;
