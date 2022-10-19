362ed99ec86f8ec1eef22d4fc801b1765245ff50
ssshun3 <s.kudo.22.7.584@4nonome.com>
2022-10-19 14:07:49 +0900

    隙間を作るcssの追加

--- src/stories/organisms-cards.stories.js
+++ src/stories/organisms-cards.stories.js
@@ -1,5 +1,5 @@
 import Item_Cards from "../components/organisms/cards/cards.html";
-
+import "../components/organisms/cards/cards.scss";
 export default { title: "Organisms/Cards" };
 
 export const Cards = () => Item_Cards;
