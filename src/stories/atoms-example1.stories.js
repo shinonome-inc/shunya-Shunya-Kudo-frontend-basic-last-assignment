b9f6bffc380baae860c7cb1052d7ae8f7b57a21e
ssshun3 <s.kudo.22.7.584@4nonome.com>
2022-12-11 17:44:30 +0900

    Atoms/Example1の作成

---
+++ src/stories/atoms-example1.stories.js
@@ -0,0 +1,6 @@
+import Item_Example from "../components/atoms/example1/example1.html";
+import "../components/atoms/example1/example1.scss";
+
+export default { title: "Atoms/Example1" };
+
+export const Example = () => Item_Example;
