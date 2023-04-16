6e60972dc317a505223a6b60036e968f7f63fcc6
ssshun3 <s.kudo.22.7.584@4nonome.com>
2023-04-16 23:33:30 +0900

    atoms/buttonの作成

---
+++ src/stories/atoms-button2.stories.js
@@ -0,0 +1,6 @@
+import Item_button2 from "../components/atoms/button2/button2.html";
+import "../components/atoms/button2/button2.scss";
+
+export default { title: "Atoms/button2" };
+
+export const button2 = () => Item_button2;
